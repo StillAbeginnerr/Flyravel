6bf4916d7b89d7384e568c0c9e25226a96d463b2
shivam agarwal <shivamagarwaloff@gmail.com>
2022-06-04 16:25:12 +0530

    q

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -3,21 +3,9 @@ function sound()
    var sond = new Audio('assets/sound/mouse.mp3');
    sond.play()
 }
-var i=0;
-var audio = new Audio('assets/sound/backgr.mp3');
-
 function windoww(){
-    if(i==0)
-    {
-        audio.play();
-        i++;
-      
-    }else{
-        delete audio;
-   
-    }
-
+    var audio = new Audio('assets/sound/backgr.mp3');
+    audio.play();
 }
-
 
 
