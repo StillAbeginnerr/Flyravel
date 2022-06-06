dc8744ed815f5cecc9abc5e13c7d0cd6b4d08c7c
shivam agarwal <shivamagarwaloff@gmail.com>
2022-06-06 18:58:40 +0530

    frontend

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -2,6 +2,7 @@ var i=0;
 function sound()
 {
    var sond = new Audio('assets/sound/mouse.mp3');
+   sond.volume = 0.015;
    sond.play()
 }
 var audio = new Audio('assets/sound/backgr.mp3');
@@ -30,3 +31,29 @@ var navToTop = function () {
 };
 
 window.addEventListener("scroll", navToTop);
+
+
+///data
+const basicAutocomplete = document.querySelector('#basic');
+const data = ['Museum', 'Party', 'Restaurant', 'Concert', 'Architecture'];
+const dataFilter = (value) => {
+  return data.filter((item) => {
+    return item.toLowerCase().startsWith(value.toLowerCase());
+  });
+};
+
+new mdb.Autocomplete(basicAutocomplete, {
+  filter: dataFilter
+});
+
+const locationAutocomplete = document.querySelector('#location');
+const dataL = ['1 adult', '2 adults', '3 adults', '2 adults and 1 child', '2 adults and 2 children'];
+const dataFilterL = (value) => {
+  return dataL.filter((item) => {
+    return item.toLowerCase().startsWith(value.toLowerCase());
+  });
+};
+
+new mdb.Autocomplete(locationAutocomplete, {
+  filter: dataFilterL
+});
