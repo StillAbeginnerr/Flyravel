9acdff544ae294394171f2a0a1a44bb5ab4eb112
shivam agarwal <shivamagarwaloff@gmail.com>
2022-06-06 12:27:59 +0530

    hero-section

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -16,3 +16,17 @@ if(i===1)
 }
 
 
+
+navtbut = document.getElementById("navtbut");
+
+var navToTop = function () {
+  var y = window.scrollY;
+  if (y >= 220) {
+      navtbut.className = "appear"
+  } 
+  else {
+      navtbut.className = "disappear"
+  }
+};
+
+window.addEventListener("scroll", navToTop);
